e772df99040524bdd93ca5425072be1b79358fd6
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-10 14:28:46 +0530

    #include<stdio.h> int main() {     int x,i,sum=0;     scanf("%d",&x);     int a[x];     for(i=0;i<x;i++)     scanf("%d",&a[i]);     for(i=0;i<x;i++)     {         if(i%2!=0)         sum=sum+a[i];     }     printf("%d",sum);
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,i,sum=0;
+    scanf("%d",&x);
+    int a[x];
+    for(i=0;i<x;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<x;i++)
+    {
+        if(i%2!=0)
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    
+    
+}
