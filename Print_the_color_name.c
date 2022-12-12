dc921595d0de86db5e491b4f9a2d5ca01a28195c
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-12 18:41:55 +0530

    #include<stdio.h> int main() {     char a;     scanf("%c",&a);     if(a=='G')     {      printf("Green");     }     else if(a=='Y')     {      printf("Yellow");
        }
        else
        {
            printf("-1");
        }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a=='G')
+    {
+     printf("Green");   
+    }
+    else if(a=='Y')
+    {
+     printf("Yellow");
+     
+    }
+    else
+    {
+        printf("-1");
+    }
+}
