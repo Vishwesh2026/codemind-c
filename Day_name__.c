7935e5522366ae2a253e5462ff5db77a915eb328
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-13 20:14:14 +0530

    #include<stdio.h> int main() {     int day;     scanf("%d", &day);     if(day < 1 || day > 7)     {         printf("invalid input ");         return 0;     }     if(day == 1)     {         printf("monday ");     }     else if(day == 2)     {         printf("tuesday ");     }     else if (day == 3)     {         printf("wednesday ");     }     else if(day == 4)     {         printf("thursday ");     }     else if(day == 5)     {         printf("friday ");     }     else if(day == 6)     {         printf("saturday ");     }     else     {         printf("sunday ");     }
        return 0;
    }

---
+++ Day_name__.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+    int day;  
+    scanf("%d", &day);
+    if(day < 1 || day > 7)
+    {
+        printf("invalid input
+");
+        return 0;
+    }
+    if(day == 1) 
+    {  
+        printf("monday
+");  
+    } 
+    else if(day == 2) 
+    {  
+        printf("tuesday
+");  
+    } 
+    else if (day == 3) 
+    {  
+        printf("wednesday
+");  
+    }
+    else if(day == 4) 
+    {  
+        printf("thursday
+");  
+    } 
+    else if(day == 5) 
+    {  
+        printf("friday
+");  
+    } 
+    else if(day == 6)
+    {  
+        printf("saturday
+");  
+    }
+    else 
+    {  
+        printf("sunday
+");  
+    }
+   
+    return 0;
+}
