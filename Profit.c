88a8003aa6fcdfb1b0cf4ceec777d0840080d42d
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-14 20:03:30 +0530

    #include<stdio.h> int main() {     int cp,sp,total;     scanf("%d%d",&cp,&sp);     if(sp>cp)     {     total=sp-cp;     printf("Profit");     }     else if(cp>sp)     {     total=cp-sp;     printf("Loss");     } }

---
+++ Profit.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+    int cp,sp,total;
+    scanf("%d%d",&cp,&sp);
+    if(sp>cp)
+    {
+    total=sp-cp;
+    printf("Profit");
+    }
+    else if(cp>sp)
+    {
+    total=cp-sp;
+    printf("Loss");
+    }
+}
