d19b9709d2b11cdbefa529818002e930f23014d7
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-14 20:00:28 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x<=35)     {         printf("FAILED");     }     else     {         printf("PASSED");     }
    }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<=35)
+    {
+        printf("FAILED");
+    }
+    else
+    {
+        printf("PASSED");
+    }
+    
+}
