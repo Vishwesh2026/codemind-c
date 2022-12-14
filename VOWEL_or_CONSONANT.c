b8d0fecf613f99c28f93fe815205ce8849a6a733
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-14 19:58:39 +0530

    #include<stdio.h> int main() {     char a,e,i,o,u,x;     scanf("%c",&x);     if( x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')     {         printf("VOWEL");     }     else {         printf("CONSONANT");     }
    }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a,e,i,o,u,x;
+    scanf("%c",&x);
+    if( x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
+    {
+        printf("VOWEL");
+    }
+    else {
+        printf("CONSONANT");
+    }
+    
+}
