8cd64e8c8692dafdc309ddd14d9833ecc3828e57
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2022-12-19 20:55:36 +0530

    #include<stdio.h> int main() {     int a,b,i,temp;     scanf("%d",&temp);     for(i=1;i<=temp;i++)     {         scanf("%d%d ",&a,&b);         printf("%d ",a+b);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,temp;
+    scanf("%d",&temp);
+    for(i=1;i<=temp;i++)
+    {
+        scanf("%d%d
+",&a,&b);
+        printf("%d
+",a+b);
+    }
+}
