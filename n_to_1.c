461d3f13f46fde8b1e6cc34a7991d59c38dda673
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2023-01-02 21:06:31 +0530

    #include<stdio.h> int main() {     int i,n;     //i = 1;     scanf("%d",&n);     i=n;     while(i>=1)     {     printf("%d ",i);     i--;     } }

--- n_to_1.c
+++ n_to_1.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
+    int i,n;
+    //i = 1;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    i=n;
+    while(i>=1)
     {
-        printf("%d ",i);
+    printf("%d ",i);
+    i--; 
     }
 }
