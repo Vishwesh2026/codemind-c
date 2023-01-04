59554d2e6baba55b3ec229c8c4a3e9e586fc106e
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2023-01-04 20:03:38 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     return 0; }

--- array_of_add_and_even_elements.c
+++ array_of_add_and_even_elements.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,s=0;
+    int n,arr[100],i,j;
     scanf("%d",&n);
-    int a[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2 !=0)
+        if(arr[i]%2!=0)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]%2==0)
+        if(arr[i]%2==0)
         {
-            printf("%d ",a[i]);
+            printf("%d ",arr[i]);
         }
     }
+    return 0;
 }
