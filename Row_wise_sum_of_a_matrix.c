c5bdd555a5008d88d0c9d574abe01586e58cf40a
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2023-01-15 19:16:46 +0530

    #include<stdio.h> int main() {     int r,c,i,j,k=0,a[100][100];     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         k=0;         for(j=0;j<c;j++)         {             k=k+a[i][j];         }         printf("%d ",k);     }
    }

--- Row_wise_sum_of_a_matrix.c
+++ Row_wise_sum_of_a_matrix.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,i,j,k=0,arr[100][100];
+    int r,c,i,j,k=0,a[100][100];
     scanf("%d%d",&r,&c);
     for(i=0;i<r;i++)
     {
         for(j=0;j<c;j++)
         {
-            scanf("%d",&arr[i][j]);
+            scanf("%d",&a[i][j]);
         }
     }
     for(i=0;i<r;i++)
@@ -15,7 +15,7 @@ int main()
         k=0;
         for(j=0;j<c;j++)
         {
-            k=k+arr[i][j];
+            k=k+a[i][j];
         }
         printf("%d ",k);
     }
